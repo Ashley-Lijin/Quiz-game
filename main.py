0ea14cfcd3851cc8b3324cd259cac1c10aa99cbd
Ashley-Lijin <ashleylijin7@gmail.com>
2023-04-17 23:21:26 +0530

    fixed(bugs)

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ score = 0
 # looping the json values
 for key, value in data.items():
     # printing the question and asking the answer to user
-    print(value["question"])
+    print('\n' +value["question"])
     answer = input('Answer ? ')
 
     '''
@@ -22,13 +22,13 @@ for key, value in data.items():
     if answer.lower() == value["answer"].lower():
         score = score + 1
         print("correct :)  ")
-        print('You score is: {} \n'.format(score))
+        print('You score is: {} '.format(score))
     else:
         print("Wrong :(")
         print("The answer is : {} ".format(value["answer"]))
-        print('You score is: {} \n'.format(score))
+        print('You score is: {}'.format(score))
 
 # total number of question correct &  percentage
 
-print('you got {} got 2 questions correctly'.format(score))
+print('\nyou got {} got 2 questions correctly'.format(score))
 print('Your percentage is {} %'.format(score/2*100))
