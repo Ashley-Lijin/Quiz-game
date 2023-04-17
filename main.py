947d326a71cbc1493e439332d490d3048498cb4c
Ashley-Lijin <ashleylijin7@gmail.com>
2023-04-17 17:44:53 +0530

    created quiz

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import json
 
-with open("data.json","r") as quiz:
+with open("data.json", "r") as quiz:
     data = json.load(quiz)
 
 print(data)
+
+
+for key, value in data.items():
+    print(value["question"])
+    answer = input('Answer ? ')
+
+    if answer.lower() == value["answer"].lower:
+        print("correct :)")
+    else:
+        print("correct :)")
+        print("The answer is : {}".format(value["answer"]))
