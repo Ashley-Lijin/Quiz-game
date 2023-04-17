6c05b86d22db36a0eac4d280637e61a6b4c669bd
Ashley-Lijin <ashleylijin7@gmail.com>
2023-04-17 18:11:44 +0530

    added(documentaion & score)

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import json
 
+# getting the data from json file
 with open("data.json", "r") as quiz:
     data = json.load(quiz)
 
-print(data)
+# for score
+score = 0
 
-
+# looping the json values
 for key, value in data.items():
     print(value["question"])
     answer = input('Answer ? ')
 
-    if answer.lower() == value["answer"].lower:
-        print("correct :)")
+    '''
+    checking weather the answer is correct or wrong
+    if answer is correct then the score will increase
+    else the score is will be the same
+    '''
+
+    if answer.lower() == value["answer"].lower():
+        score = score + 1
+        print("correct :)  ")
+        print('You score is: {} \n'.format(score))
     else:
-        print("correct :)")
-        print("The answer is : {}".format(value["answer"]))
+        print("Wrong :(")
+        print("The answer is : {} \n ".format(value["answer"]))
+        print('You score is: {} \n'.format(score))
+
+print('you got {} got 2 questions correctly'.format(score))
+print('Your percentage is {} %'.format(score/2*100))
